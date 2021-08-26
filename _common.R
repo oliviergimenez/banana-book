837f786a0d49737072c3d324f0ed441e7539303d
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2021-08-26 21:05:04 +0300

    Rough draft of chapters 1, 2, and 3.

--- _common.R
+++ _common.R
@@ -1,8 +1,18 @@
+# packages
+library(tidyverse)
+theme_set(theme_light())
+library(nimble)
+library(MCMCvis)
+library(magick)
+library(pdftools)
+
 # example R options set globally
 options(width = 60)
 
 # example chunk options set globally
 knitr::opts_chunk$set(
   comment = "##",
-  collapse = TRUE
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE
   )
